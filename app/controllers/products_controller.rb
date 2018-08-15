729299f4e11d760e46a52181f36fae9fe83a6ac3
Joey Saha <joeysaha@hotmail.com>
2018-08-15 18:20:59 -0400

    minor edit to see if this works

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,7 +15,6 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new
-
     @product.name = params[:product][:name]
     @product.description = params[:product][:description]
     @product.price_in_cents = params[:product][:price_in_cents]
