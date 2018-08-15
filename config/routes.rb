580edf194fcfa95b6ee6f9700cb6671e508e1680
curtischatch <curtis.chat.hk@gmail.com>
2018-08-15 16:23:30 -0400

    nested reviews done

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-  resources :products
+  resources :products do
+
+    resources :reviews, except: [:index, :new, :show]
+
+  end
 
   root 'products#index'
   get '/products' => 'products#index'
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
