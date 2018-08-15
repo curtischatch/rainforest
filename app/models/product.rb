8c59a5e48287854579c0aba38bc4f2cab34fa08d
curtischatch <curtis.chat.hk@gmail.com>
2018-08-15 16:06:27 -0400

    generated reviews model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,14 @@
 class Product < ApplicationRecord
 
+  has_many :reviews
+
   validates :name, :description, :price_in_cents, presence: true
   validates :price_in_cents, numericality: true
+
+  def price_in_dollars
+    price_in_dollars = price_in_cents.to_f / 100
+    sprintf("$%.2f", price_in_dollars)
+  end
+
 
 end
