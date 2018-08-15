222593cdb186979b84fabdcbe7e3e41e9d9891aa
curtischatch <curtis.chat.hk@gmail.com>
2018-08-15 11:22:45 -0400

    added to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+p1 = Product.create({ name: 'water bottle', description: 'stores water', price_in_cents: 20})
+
+p2 = Product.create({ name: 'joey', description: 'stores water', price_in_cents: 80})
+
+p3 = Product.create({ name: 'rob', description: 'stores beer', price_in_cents: 5})
